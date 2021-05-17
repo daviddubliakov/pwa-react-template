ff141382022731f21f6e10519152b551e6be02a1
David Dubliakov <dubliakov1@gmail.com>
2021-05-17 10:26:08 +0300

    removed styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-const App = () => (<h1>Happy hacking</h1>);
+const App = () => (
+  <Navbar bg="primary" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+  </Navbar>
+);
 
 export default App;
